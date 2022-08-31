51e434437766428e63bb06b88e498da71a967fde
s4180t <s4180t@yandex.ru>
2022-08-31 13:52:17 +0300

    Initial commit

---
+++ controller/ObjectPageBlockBase.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define(["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/Controller"], function (JSONModel, Controller) {
+    "use strict";
+
+    return Controller.extend("sap.uxap.sample.ObjectPageBlockBase.controller.ObjectPageBlockBase", {
+        onInit: function () {
+            this.oJsonConfigModel = new JSONModel({ subSectionLayout: "TitleOnTop" });
+            this.getView().setModel(this.oJsonConfigModel, "ConfigModel");
+            this.isTitleOnTop = true;
+            this.byId("ObjectPageLayout")
+                .getSections()
+                .forEach(function (oSection) {
+                    oSection.getSubSections().forEach(function (oSubSection) {
+                        oSubSection.setMode("Expanded");
+                        oSubSection.getBlocks().forEach(function (oBlock) {
+                            oBlock.setMode("Expanded");
+                        });
+                    });
+                });
+        },
+
+        toggleTitle: function () {
+            this.isTitleOnTop = !this.isTitleOnTop;
+            this.oJsonConfigModel.setProperty("/subSectionLayout", this.isTitleOnTop ? "TitleOnTop" : "TitleOnLeft");
+        }
+    });
+});
